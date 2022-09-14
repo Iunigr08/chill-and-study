09cb2b857ab6d23c0d92b477c18d69e0a3cbfb55
Shota Sato <last11142@gmail.com>
2022-09-14 22:36:32 +0900

    [update] homelayout

--- src/components/Flame/HomeLayout.vue
+++ src/components/Flame/HomeLayout.vue
@@ -51,27 +51,18 @@ export default {
   display: grid;
   grid-template-columns: min-content 1fr;
   grid-template-rows: 5rem 1fr;
-  /* grid-template-areas: "header header" "side-bar main"; */
 }
 .header {
-  /* grid-area: "header"; */
   grid-row: 1/2;
   grid-column: 1/3;
   width: 100%;
   height: 5rem;
-  /* border: #000; */
 }
 .sidebar {
   position: relative; /* after要素の基準 */
-  /* grid-area: "side-bar"; */
   grid-row: 2/3;
   grid-column: 1/2;
-  /* サイドバーのwidthは別クラスで実装（メニューフォールド処理のため） */
-  /* width: 26rem; */
-  /* width: 25rem; */
   height: 100%;
-  /* height: calc(100% - 2rem); */
-  /* margin: 1rem 0; */
   font-size: 2rem;
   transition: 0.2s;
 }
@@ -81,17 +72,12 @@ export default {
   top: 2rem;
   right: 2rem;
 
-  /* shaping */
-  /* width: 1.8rem; */
-  /* width: 86%; */
   height: calc(100% - 4rem);
 
   /* mophing */
   background-color: #bfbfbf;
   border-radius: 4px;
   background: #e1d6d6;
-  /* box-shadow: 8px 8px 17px #b9afaf, -8px -8px 17px #fffdfd; */
-  /* box-shadow: -5px 5px 10px #d1c7c7, 5px -5px 10px #f1e5e5; */
   box-shadow: -5px 5px 12px #b2a9a9, 5px -5px 12px #ffffff;
 }
 .menu-folded {
@@ -101,12 +87,9 @@ export default {
   width: 25rem;
 }
 .main-content {
-  /* grid-area: "main"; */
   grid-row: 2/3;
   grid-column: 2/3;
-  /* width: 20vw; */
   height: 100%;
-  /* border: 1px solid pink; */
   font-size: 2rem;
 }
 </style>
